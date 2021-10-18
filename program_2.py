c8fea430533c51c5605e459bfece5948d1b14dde
bednie <88800117+bednie@users.noreply.github.com>
2021-10-18 15:34:47 -0600

    Handle user-supplied filenames

--- program_2.py
+++ program_2.py
@@ -1,6 +1,6 @@
 from os.path import exists
 
-sum_of_integers = 0 
+sum_of_integers = 0
 number_of_integers = 0 
 average_of_integers = 0
 list_of_integers = []
@@ -21,7 +21,7 @@ if exists(user_filename):
 
 elif user_filename == "" and exists('LotsOfNumbers.txt'):
     print('LotsOfNumbers.txt exists')
-    # TODO create file object with LotsOfNumbers.txt 
+    user_filename = 'LotsOfNumbers.txt'
 
 else:
     print('File not found. Please run the program again.')
@@ -30,7 +30,9 @@ else:
 
 file = open(user_filename, 'r')
 for line in file:
-    list_of_integers.append(int(file.read()))
+    #print(line.strip())
+    list_of_integers.append(int(line.strip()))
+    print(list_of_integers)
 
 # calculate sum, average, and count of integers in file
 number_of_integers = len(list_of_integers)
@@ -39,8 +41,8 @@ average_of_integers = sum_of_integers / number_of_integers
 
 # output file to 'Program2.out'
 with open('Program2.out', 'w') as f:
+    f.write('Name of file: ' + user_filename + '\n')
     f.write('Sum of integers in file: ' + str(sum_of_integers) + '\n')
     f.write('Average of integers in file: ' + str(average_of_integers) + '\n')
     f.write('Count of integers in file: ' + str(number_of_integers) + '\n')
     f.close()
-
